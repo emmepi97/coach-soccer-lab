5aae06a8a06776b7495b09129e60a95af42eaf37
emmepi97 <matteopoggesi23@gmail.com>
2026-06-26 17:28:45 +0200

    Delete App.jsx

--- App.jsx
+++
@@ -1,3 +0,0 @@
-import { selectRows, insertRow, updateRow } from './baseService';
-export async function getFeatureFlags(){ return selectRows('feature_flags', { order:{column:'feature_key', ascending:true} }); }
-export async function saveFeatureFlag(flag){ return flag.id ? updateRow('feature_flags', flag.id, flag) : insertRow('feature_flags', flag); }
