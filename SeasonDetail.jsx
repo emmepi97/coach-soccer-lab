d6adaabdcb77b5a9b71f5652061782628b20579b
emmepi97 <matteopoggesi23@gmail.com>
2026-06-26 17:45:45 +0200

    Delete SeasonDetail.jsx

--- SeasonDetail.jsx
+++
@@ -1,7 +0,0 @@
-import CrudSection from './CrudSection';
-import { getIpoEventTypes, createIpoEventType, updateIpoEventType, deleteIpoEventType } from '../services/ipoEventsService';
-const fields = [{"name": "event_name", "label": "Event Name"}, {"name": "default_ipo_score", "label": "Default Ipo Score"}, {"name": "description", "label": "Description"}, {"name": "active", "label": "Active"}];
-const columns = [{"key": "event_name", "label": "Event Name"}, {"key": "default_ipo_score", "label": "Default Ipo Score"}, {"key": "active", "label": "Active"}];
-export default function IpoEventsSection({activeSeason,userDefaults={}, locked=false}){
- return <CrudSection title="Eventi IPO" description="Gestione dei tipi evento e punteggi IPO proposti automaticamente." fields={fields} columns={columns} load={getIpoEventTypes} create={createIpoEventType} update={updateIpoEventType} remove={deleteIpoEventType} defaults={userDefaults} locked={locked} />;
-}
